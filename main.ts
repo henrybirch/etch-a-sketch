35843500e1a16324ca5ba161e4a204fd25c64a82
henrybirch <hab.b@hotmail.co.uk>
2023-02-21 18:59:03 +0000

    Add layGrid and helper functions

--- main.ts
+++ main.ts
@@ -10,3 +10,47 @@ function getGridUnit(size: number, column: number, row: number) {
     div.style.height = sizeVh
     return div
 }
+
+function layGrid(size: number) {
+    const gridUnitSize = 80 / size
+    const pad = document.getElementById("pad")
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            pad.appendChild(getGridUnit(gridUnitSize, j, i))
+        }
+    }
+}
+
+function scaleBetween(unscaledNum, minAllowed, maxAllowed, min, max) {
+    return (maxAllowed - minAllowed) * (unscaledNum - min) / (max - min) + minAllowed;
+}
+
+function listenForMouse(column: number, row: number) {
+    const gridUnit = document.getElementById(column.toString() + "," + row.toString())
+
+    const passover = gridUnit.getAttribute("data-passovers")
+    const newPassover = (Number(passover) + 1)
+    const newShade = (newPassover > 10 ? 1 : scaleBetween(newPassover, 0, 1, 0, 10 ))
+    gridUnit.style.backgroundColor = `rgba(0, 0, 0, ${newShade})`
+
+    gridUnit.setAttribute("data-passovers", newPassover.toString())
+    gridUnit.classList.add("selected")
+}
+
+function setGridListeners() {
+    const gridUnits = document.querySelectorAll(".grid-unit")
+    gridUnits.forEach(el => {
+        const column = Number(el.getAttribute("data-column"))
+        const row = Number(el.getAttribute("data-row"))
+
+        el.addEventListener("mouseover", e => listenForMouse(column, row))
+    })
+}
+
+function reset(n: number) {
+    layGrid(n)
+    setGridListeners()
+}
+
+reset(20)
